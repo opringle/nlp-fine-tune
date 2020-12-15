5f3f569dce939d12c34d3c530b70ca74edb491d4
Oliver Pringle <oliver.pringle@1qbit.com>
2020-12-15 12:01:54 -0800

    update training logic

--- trainer/task.py
+++ trainer/task.py
@@ -1,9 +1,54 @@
+import argparse
+import logging
+
 from datasets import load_dataset
-from transformers import RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification
+from transformers import RobertaTokenizer
+
 import torch
-from torch import optim
-from torch.utils.data import Dataset
+from torch.nn import CrossEntropyLoss
 from tqdm import tqdm
+
+from .model import TorchTextClassifier
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='GCP training application')
+    
+    group = parser.add_argument_group('data')
+    group.add_argument('--train-data-file', required=True)
+
+    group = parser.add_argument_group('hyperparameters')
+    group.add_argument('--epochs', type=int, required=True)
+    group.add_argument('--batch-size', type=int, required=True)
+
+    group = parser.add_argument_group('compute')
+    group.add_argument(
+        '--distribution-strategy', 
+        type=str, 
+        default=None,
+        choices=[
+            'MirroredStrategy',
+            'MultiWorkerMirroredStrategy'
+        ],
+    )
+    
+    group = parser.add_argument_group('artifacts')
+    group.add_argument(
+        '--job-dir',
+        required=True,
+        type=str,
+        help='GCS location to write checkpoints and export models.',
+    )
+    group.add_argument(
+        '--reuse-job-dir',
+        action='store_true',
+        default=False,
+        help="""
+        Flag to decide if the model checkpoint should be
+        re-used from the job-dir.
+        If set to False then the job-dir will be deleted.
+        """
+    )
+    return parser.parse_args()
 
 
 def get_dataloader(dataset, tokenizer, batch_size=32):
@@ -15,9 +60,12 @@ def get_dataloader(dataset, tokenizer, batch_size=32):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    args = parse_args()
+    
     MODEL_NAME = 'roberta-large'
-    EPOCHS = 30
-    BATCH_SIZE = 32
+    EPOCHS = args.epochs
+    BATCH_SIZE = args.batch_size
 
     # load, encode and format input data for pytorch model
     datasets = load_dataset('ag_news')
@@ -27,19 +75,22 @@ if __name__ == '__main__':
     train_dataloader = get_dataloader(train_ds, tokenizer, batch_size=BATCH_SIZE)
     test_dataloader = get_dataloader(test_ds, tokenizer, batch_size=BATCH_SIZE)
 
-    model = RobertaForSequenceClassification.from_pretrained(MODEL_NAME)
 
-    # fine tune the model on the new dataset
+    model = TorchTextClassifier(num_classes=4)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-5)
+    loss_fn = CrossEntropyLoss()
+
+    # fit the model to the data
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.train().to(device)
-    optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-5)
     for epoch in range(EPOCHS):
         for i, batch in enumerate(tqdm(train_dataloader)):
-            batch = {k: v.to(device) for k, v in batch.items()}
-            outputs = model(**batch)
-            loss = outputs[0]
+            features = {k: v.to(device) for k, v in batch.items() if k != 'labels'}
+            labels = batch['labels'].to(device)
+            predictions = model(**features)
+            loss = loss_fn(predictions, labels)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             if i % 10 == 0:
-                print(f"loss: {loss}")
+                logging.info("Epoch {} batch {} loss={:.3f}".format(epoch, i, loss))
